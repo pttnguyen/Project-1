a50935966330883907e5c90587c7e3b05e9cdf6f
Peter Nguyen <ptt.nguyen@gmail.com>
2012-09-18 22:58:04 -0700

    Peter's Additions
    Added styles to main.css, functions to main.js, and added sandbox.html

--- js/main.js
+++ js/main.js
@@ -1,23 +1,33 @@
 $(document).ready(function() {
 
-  $('#search').click(function() {
+  $('#fetch').submit(function() {
 
     var t = $('#term').val();
-    alert(t);
+    // alert(t);
+	
     var theHtml="";
-
+	
+	$("ul#poster").empty();
+	
     $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
               {tags: t, tagmode: "any", format: "json"},
               function(data) {
                 $.each(data.items, function(i,item) {
-                  theHtml += '<li><a href="' + item.media.m + '" target="_blank">';
-                  theHtml += '<img height="40" width="auto" title="' + item.title +
+                  theHtml += '<li class="thumb"><a href="' + item.media.m + '" target="_blank">';
+                  theHtml += '<img height="75px" width="auto" title="' + item.title +
                     '" src="' + item.media.m + '" alt="' + item.title + '" />';
                   theHtml += '</a></li>';
                 });
                 $("ul#poster").append(theHtml);
+				$("ul#poster li").draggable({revert: true});
               });
-    $('#poster li').draggable({ revert: true} );
-  });
   return false;
-});
+	});
+
+  $("#ul#poster, ul#new-trail").sortable({
+	connectWith: ".connectedSortable"
+  });
+	
+  });
+  
+
