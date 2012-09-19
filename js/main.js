ee6e0521e36b678bbee7b4d89436e25ce5aa1255
Brian Cavagnolo <bcavagnolo@berkeley.edu>
2012-09-19 16:11:08 -0700

    recover main.css and main.js to previous state

--- js/main.js
+++ js/main.js
@@ -1,44 +1,3 @@
-<<<<<<< HEAD
-$(document).ready(function() {
-  
-  $('#fetch').submit(function() {
-
-    var t = $('#term').val();
-    // alert(t);
-	
-    var theHtml="";
-	
-	$("#poster").empty();
-	$('#term').val() == "";
-	
-    $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
-              {tags: t, tagmode: "any", format: "json"},
-              function(data) {
-                $.each(data.items, function(i,item) {
-                  theHtml += '<li class="thumb"><a href="' + item.media.m + '" target="_blank">';
-                  theHtml += '<img height="75px" width="auto" title="' + item.title +
-                    '" src="' + item.media.m + '" alt="' + item.title + '" />';
-                  theHtml += '</a></li>';
-                });
-                $("#poster").append(theHtml);
-              });
-  return false;
-  
-	});
-	
-	
-	
-	$(function() {
-  $("#poster, #new-trail").sortable({
-	connectWith: ".connectedSortable",
-	revert: true
-	});
-		});
-	
-  });
-  
-
-=======
 $(document).ready(function() {
   $("#poster, #new-trail").sortable({
 	connectWith: ".pic-list"
@@ -64,4 +23,3 @@ $(document).ready(function() {
     return false;
   });
 });
->>>>>>> ac7488d17c9e9892a15b8bd24348c42b162222b0
